80d7f298881ef66d8764b07cab3058b06b80c906
BenL19 <g00371775@gmit.ie>
2020-02-27 14:35:04 +0000

    Encapsulation

--- Main.java
+++ Main.java
@@ -1,3 +1,19 @@
 public class Main {
     // Main method
+    public static void main(String[] args) {
+        Person personobj1 = new Person();
+
+        personobj1.setfirstName("Jimmy");
+
+        System.out.println(personobj1.getfirstName());
+
+        personobj1.setmiddleNames("Jim");
+
+        System.out.println(personobj1.getmiddleNames());
+
+        personobj1.setuserName("JJJJJJJJJJJJJJJJJJJJ22");
+
+        System.out.println(personobj1.getuserName());
+    }
+    
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,41 @@
-class Person {
+public class Person {
     // Instance Variables
+    private String firstName;
+    private String[] middleNames;
+    private String userName;
     
     // Methods
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getmiddleNames() {
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < middleNames.length; i++){
+            sb.append(middleNames[i] + " ");
+        }
+        String middleNames = sb.toString();
+        return middleNames;
+    }
+
+    public void setmiddleNames(String middleNames) {
+        this.middleNames = middleNames.split(" ");
+    }
+
+    public String getuserName() {
+        return userName;
+    }
+
+    public void setuserName(String userName) {
+        if(userName.length() > 10){
+            this.userName = userName.substring(0, 10);
+        }else{
+            this.userName = userName;
+        }
+    }
+
 }
